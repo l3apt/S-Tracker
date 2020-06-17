d9f2cf8831eb0dd77ea2e6e0b254b76891d904d3
Baptiste <baptiste-g@hotmail.fr>
2020-06-17 11:07:56 +0200

    premier commit de test

---
+++ baniere.php
@@ -0,0 +1,9 @@
+
+  	<img id ="baniere_image" src="img/baniere.jpg" alt="baniere" title="Running Tracker - Bienvenue" />
+ 		
+ 	<ul id= "baniere_nav">
+ 			<li><a href="Home.php">Home</a></li>
+            <li><a href="gestionTracks.php">Mes tracks</a></li>
+            <li><a href="stats.php">Statistiques</a></li>
+            <li><a href="circuits.php">Mes circuits</a></li>
+	</ul>	
